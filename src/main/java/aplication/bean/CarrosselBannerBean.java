1d3196fc998c197244cd055fe6e26fbe5fda78fb
d0uglas77p <douglas_1313@hotmail.com>
2024-12-30 17:49:12 -0300

    updade

---
+++ src/main/java/aplication/bean/CarrosselBannerBean.java
@@ -0,0 +1,26 @@
+package aplication.bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import java.util.ArrayList;
+import java.util.List;
+
+@ManagedBean
+@ViewScoped
+public class CarrosselBannerBean {
+    private List<String> listaBanner;
+
+    public CarrosselBannerBean() {
+        listaBanner = new ArrayList<>();
+        listaBanner.add("/resources/images/banner1.png");
+        listaBanner.add("/resources/images/banner2.png");
+    }
+
+    public List<String> getListaBanner() {
+        return listaBanner;
+    }
+
+    public void setListaBanner(List<String> listaBanner) {
+        this.listaBanner = listaBanner;
+    }
+}
--- src/main/java/aplication/bean/CarrosselBean.java
+++ src/main/java/aplication/bean/CarrosselBean.java
@@ -1,4 +1,30 @@
 package aplication.bean;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import java.util.ArrayList;
+import java.util.List;
+
+@ManagedBean
+@ViewScoped
 public class CarrosselBean {
+    private List<String> listaImagens;
+
+    public CarrosselBean() {
+        listaImagens = new ArrayList<>();
+        listaImagens.add("/resources/images/1.png");
+        listaImagens.add("/resources/images/2.png");
+        listaImagens.add("/resources/images/3.png");
+        listaImagens.add("/resources/images/4.png");
+        listaImagens.add("/resources/images/5.png");
+
+    }
+
+    public List<String> getListaImagens() {
+        return listaImagens;
+    }
+
+    public void setListaImagens(List<String> listaImagens) {
+        this.listaImagens = listaImagens;
+    }
 }
